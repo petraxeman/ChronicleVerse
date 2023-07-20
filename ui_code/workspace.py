76eb22acf93e81ce0240c969ac90221e20b9838b
petraxeman <pashamanprofirus@gmail.com>
2023-07-20 18:33:18 +0300

    [0.0.7.1] Create table tab 1 phase

--- ui_code/workspace.py
+++ ui_code/workspace.py
@@ -10,6 +10,8 @@ from kivy.metrics import dp
 from kivy.uix.screenmanager import Screen
 from kivymd.uix.tab.tab import MDTabsBase
 from kivymd.uix.scrollview import MDScrollView
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.gridlayout import MDGridLayout
 from kivymd.icon_definitions import md_icons
 from kivymd.font_definitions import fonts
 from widgets import MDTextFieldFuncIcon
@@ -20,13 +22,38 @@ import tkinter as tk
 
 
 Builder.load_file('./ui/workspace.kv')
+Builder.load_file('./ui/work_with_tables.kv')
 
 class Tab(BoxLayout, MDTabsBase):
     pass
 
+
+class CreateTableTab(MDScrollView, MDTabsBase):
+    def __init__(self, **kwargs):
+        super(CreateTableTab, self).__init__(**kwargs)
+        #self.do_scroll_x = True
+        #self.do_scroll_y = False
+        self.cols = 3
+        self.rows = 2
+
+    def _add_new_field(self, **kwargs):
+        self.ids.field_container.add_widget(FieldsContainer())
+
+
+class FieldsContainer(MDGridLayout):
+    def __init__(self, **kwargs):
+        super(FieldsContainer, self).__init__(**kwargs)
+        self.height = self.minimum_height
+
+    def get_data(self):
+        return True
+
+
+
 class SettingsTab(MDScrollView, MDTabsBase):
     def __init__(self, *args, **kwargs):
         super(SettingsTab, self).__init__(*args, **kwargs)
+        self.custom_parent = None
         self._build_menus()
         self._render_delete_table()
         self._render_import_template()
@@ -50,6 +77,9 @@ class SettingsTab(MDScrollView, MDTabsBase):
 
     def _choice_table_to_delete(self):
         print(2)
+    
+    def _open_create_table_tab(self, **kwargs):
+        self.custom_parent._open_create_table_tab()
 
 
 class AssistantTab(MDScrollView, MDTabsBase):
@@ -63,11 +93,16 @@ class WorkspaceWindow(Screen):
         self.ids.nav_bar.set_state('open')
     
     def _open_settings_tab(self, **kwargs) -> None:
-        self.ids.tabs.add_widget(SettingsTab(title=utils.build_reftitle('Настройки')))
+        widget = SettingsTab(title=utils.build_reftitle('Настройки'))
+        widget.custom_parent = self
+        self.ids.tabs.add_widget(widget)
     
     def _open_assistant_tab(self, **kwargs) -> None:
         self.ids.tabs.add_widget(AssistantTab(title=utils.build_reftitle('Ассистент')))
     
+    def _open_create_table_tab(self, **kwargs) -> None:
+        self.ids.tabs.add_widget(CreateTableTab(title=utils.build_reftitle('Создание таблицы')))
+
     def _exit(self, **kwargs) -> None:
         self.manager.current = 'choice_vault'
     
